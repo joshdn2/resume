5203aa2cb6c1afa5125a4668cc27f40b44dd1b59
joshdn2 <joshdn2@LAPTOP-8JDD7T46>
2020-09-24 19:28:08 -0600

    initialize

--- myresumewebsite/src/App.js
+++ myresumewebsite/src/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import * as bs from 'react-bootstrap';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Container, Row, Col} from 'react-bootstrap'
 
 function App() {
   return (
-    <bs.Container>
-      <bs.Row>
-        <bs.Col>
-        </bs.Col>
-      </bs.Row>
-    </bs.Container>
+    <Container>
+      <Row>
+        <Col>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
