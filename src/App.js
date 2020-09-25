ae5e5d21cec3a270060c33fa859f242e0004f84b
joshdn2 <joshdn2@LAPTOP-8JDD7T46>
2020-09-25 09:46:36 -0600

    progress on layout

--- src/App.js
+++ src/App.js
@@ -3,25 +3,32 @@ import logo from './logo.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {Container, Row, Col} from 'react-bootstrap'
-
+import myLogo from "./logopic.png"
 export default function App() {
 
+  let arrLinks = [
+    {name:'INTRODUCTION',link:'introduction'},
+    {name:'ABOUT ME',link:'about-me'},
+    {name:'EXPERIENCE',link:'experience'},
+    {name:'CONTACT',link:'contact'}
+  ]
   return (
-    <Container fluid noGutters style={{paddingLeft:"0px", paddingRight:"0px"}}>
-      <Row noGutters >
-        <Col noGutters md="3">
-          <div className="sidebar">
-            <h2>
-            Josh Danneman
-            </h2>
-          </div>
-        </Col>
-        <Col md="9" noGutters className="middleBody">
-          <div>
-
-          </div>
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <div className="sidebar">
+        <img src={myLogo} className="logopic"/>
+        {arrLinks.map((l) => {
+          return (
+            <div className="linkSection">
+              <h5 className="linkText">{l.name}</h5>
+            </div>
+          )
+        })}
+      </div>
+      <div className="notSidebar">
+        <Container>
+          Content goes here
+        </Container>
+      </div>
+    </>
   )
 }
