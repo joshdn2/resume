214876d4b2f1986039fd03c9df7dd48d56fde84c
joshdn2 <joshdn2@LAPTOP-8JDD7T46>
2020-09-24 22:56:48 -0600

    set up pallete and sidebar layout!

--- src/App.js
+++ src/App.js
@@ -4,23 +4,24 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {Container, Row, Col} from 'react-bootstrap'
 
-function App() {
+export default function App() {
 
   return (
-    <Container fluid noGutters className="high">
-      <Row style={{height:"100%"}}>
-        <Col md="3">
+    <Container fluid noGutters style={{paddingLeft:"0px", paddingRight:"0px"}}>
+      <Row noGutters >
+        <Col noGutters md="3">
           <div className="sidebar">
             <h2>
             Josh Danneman
             </h2>
           </div>
         </Col>
-        <Col md="9"></Col>
+        <Col md="9" noGutters className="middleBody">
+          <div>
+
+          </div>
+        </Col>
       </Row>
     </Container>
-    
   )
 }
-
-export default App
