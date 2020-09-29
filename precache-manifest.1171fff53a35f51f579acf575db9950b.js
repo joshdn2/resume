1fc4aaf20c0b8ac9a3340f26014eca6635cbc6aa
joshdn2 <joshdn2@LAPTOP-8JDD7T46>
2020-09-28 21:53:55 -0600

    Updates

---
+++ precache-manifest.1171fff53a35f51f579acf575db9950b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fa835db551925cb00e7560c3218fc33",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "60286229a4988e9a9c46",
+    "url": "/resume/static/css/2.1e8bf307.chunk.css"
+  },
+  {
+    "revision": "28249b0017faaa78f9c5",
+    "url": "/resume/static/css/main.50094d12.chunk.css"
+  },
+  {
+    "revision": "60286229a4988e9a9c46",
+    "url": "/resume/static/js/2.6bef3a2d.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/resume/static/js/2.6bef3a2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28249b0017faaa78f9c5",
+    "url": "/resume/static/js/main.a2e456d7.chunk.js"
+  },
+  {
+    "revision": "e9137388e0a6c134b474",
+    "url": "/resume/static/js/runtime-main.525e03e0.js"
+  },
+  {
+    "revision": "a1c932240fd73800afb4ace46144900b",
+    "url": "/resume/static/media/DannemanJoshua_Online.a1c93224.jpg"
+  },
+  {
+    "revision": "f922470d5d9cfdca22da98340567b9f2",
+    "url": "/resume/static/media/Josh_Danneman_Resume.f922470d.pdf"
+  },
+  {
+    "revision": "1c6b9351973d3119ad1a4ea883391afe",
+    "url": "/resume/static/media/logopic.1c6b9351.png"
+  },
+  {
+    "revision": "a99cafea4a977ff24342c8e978114ce0",
+    "url": "/resume/static/media/phone.a99cafea.png"
+  },
+  {
+    "revision": "6bd3663e8804f4736db77a9710ef9766",
+    "url": "/resume/static/media/suitNoBackground.6bd3663e.png"
+  }
+]);
